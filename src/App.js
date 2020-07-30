c5f257d65867a124f52cda5e907085828c962cf8
EliKarpinsky <eli@relaymedical.com>
2020-07-30 15:02:30 -0400

    Sidebar navs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/navigation";
@@ -18,18 +19,30 @@ function App() {
                 <div className="header">
                     <Navigation/>
                 </div>
-                <div id={"app-container"} className={"container-fluid"}>
-                    <Switch>
-                        <Route path="/dashboard">
-                            <Dashboard />
-                        </Route>
-                        <Route path="/browse">
-                            <Browse />
-                        </Route>
-                        <Route path="/profile">
-                            <Profile />
-                        </Route>
-                    </Switch>
+                <div className={"row"}>
+                    <div id={"sidebar"} className={"col-1 pr-0"}>
+                        <Nav className="flex-column">
+                                <Nav.Link className={window.location.pathname==="/dashboard"?"nav-item nav-selected py-1":"nav-item py-1"} href="/dashboard">Dashboard</Nav.Link>
+                                <Nav.Link className={window.location.pathname==="/browse"?"nav-item nav-selected py-1":"nav-item py-1"} href="/browse">Browse</Nav.Link>
+                                <Nav.Link className={window.location.pathname==="/profile"?"nav-item nav-selected py-1":"nav-item py-1"} href="/profile">Profile</Nav.Link>
+
+                        </Nav>
+                    </div>
+                    <div className={"col-11"}>
+                        <div id={"app-container"} className={"container-fluid"}>
+                            <Switch>
+                                <Route path="/dashboard">
+                                    <Dashboard />
+                                </Route>
+                                <Route path="/browse">
+                                    <Browse />
+                                </Route>
+                                <Route path="/profile">
+                                    <Profile />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Router>
